fc9f8df9e46ff8443e8b736a22914fda81158dc7
Swouterke <wvercruysse45@gmail.com>
2018-04-04 15:38:48 +0200

    Add project files.

---
+++ TelefoonWindow/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelefoonWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public List<Persoon> personen = new List<Persoon>();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            personen.Add(new Persoon("anne", "123", "Vrienden", new BitmapImage(new Uri(@"images/anne.jpg", UriKind.Relative))));
+            personen.Add(new Persoon("bob", "456", "Vrienden", new BitmapImage(new Uri(@"images/bob.jpg", UriKind.Relative))));
+            personen.Add(new Persoon("collega1", "789", "Werk", new BitmapImage(new Uri(@"images/collega1.jpg", UriKind.Relative))));
+            personen.Add(new Persoon("collega2", "10", "Werk", new BitmapImage(new Uri(@"images/collega2.jpg", UriKind.Relative))));
+            personen.Add(new Persoon("collega3", "11", "Werk", new BitmapImage(new Uri(@"images/collega3.jpg", UriKind.Relative))));
+            personen.Add(new Persoon("ed", "12", "Vrienden", new BitmapImage(new Uri(@"images/ed.jpg", UriKind.Relative))));
+            personen.Add(new Persoon("grotezus", "13", "Familie", new BitmapImage(new Uri(@"images/grotezus.jpg", UriKind.Relative))));
+            personen.Add(new Persoon("kleinzus", "14", "Familie", new BitmapImage(new Uri(@"images/kleinzus.jpg", UriKind.Relative))));
+            personen.Add(new Persoon("tantenon", "15", "Familie", new BitmapImage(new Uri(@"images/tantenon.jpg", UriKind.Relative))));
+            personen.Add(new Persoon("vader", "16", "Familie", new BitmapImage(new Uri(@"images/vader.jpg", UriKind.Relative))));
+        }
+    }
+}
---
+++ TelefoonWindow/Persoon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace TelefoonWindow
+{
+    public class Persoon
+    {
+        public string Naam { get; set; }
+        public string Telefoonnr { get; set; }
+        public string Groep { get; set; }
+        public BitmapImage Foto { get; set; }
+
+        public Persoon(string nNaam, string nTelefoonnr, string nGroep, BitmapImage nFoto)
+        {
+            Naam = nNaam;
+            Telefoonnr = nTelefoonnr;
+            Groep = nGroep;
+            Foto = nFoto;
+        }
+    }
+}
